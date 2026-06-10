116fead17670d7ed9a87c4b56a07c43156a65f2d
RJosuCH <robertsjosue254369@gmail.com>
2026-06-10 17:51:03 -0600

    Ruta rapida integrada con Dijkstra

--- Proyecto_EDII_Avila_Cortes_Henriquez/frmRutaRapida.Designer.cs
+++ Proyecto_EDII_Avila_Cortes_Henriquez/frmRutaRapida.Designer.cs
@@ -50,26 +50,29 @@
             // cbxOrigen
             // 
             cbxOrigen.FormattingEnabled = true;
-            cbxOrigen.Location = new Point(27, 68);
+            cbxOrigen.Location = new Point(39, 113);
+            cbxOrigen.Margin = new Padding(4, 5, 4, 5);
             cbxOrigen.Name = "cbxOrigen";
-            cbxOrigen.Size = new Size(200, 23);
+            cbxOrigen.Size = new Size(284, 33);
             cbxOrigen.TabIndex = 0;
             // 
             // cbxDestino
             // 
             cbxDestino.FormattingEnabled = true;
-            cbxDestino.Location = new Point(420, 68);
+            cbxDestino.Location = new Point(600, 113);
+            cbxDestino.Margin = new Padding(4, 5, 4, 5);
             cbxDestino.Name = "cbxDestino";
-            cbxDestino.Size = new Size(200, 23);
+            cbxDestino.Size = new Size(284, 33);
             cbxDestino.TabIndex = 1;
             // 
             // lblOrigen
             // 
             lblOrigen.AutoSize = true;
             lblOrigen.Font = new Font("Segoe UI", 12F);
-            lblOrigen.Location = new Point(27, 33);
+            lblOrigen.Location = new Point(39, 55);
+            lblOrigen.Margin = new Padding(4, 0, 4, 0);
             lblOrigen.Name = "lblOrigen";
-            lblOrigen.Size = new Size(148, 21);
+            lblOrigen.Size = new Size(230, 32);
             lblOrigen.TabIndex = 2;
             lblOrigen.Text = "Seleccione el origen";
             // 
@@ -77,9 +80,10 @@
             // 
             lblDestino.AutoSize = true;
             lblDestino.Font = new Font("Segoe UI", 12F);
-            lblDestino.Location = new Point(420, 33);
+            lblDestino.Location = new Point(600, 55);
+            lblDestino.Margin = new Padding(4, 0, 4, 0);
             lblDestino.Name = "lblDestino";
-            lblDestino.Size = new Size(154, 21);
+            lblDestino.Size = new Size(240, 32);
             lblDestino.TabIndex = 3;
             lblDestino.Text = "Seleccione el destino";
             // 
@@ -87,9 +91,10 @@
             // 
             lblTitulo.AutoSize = true;
             lblTitulo.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblTitulo.Location = new Point(287, 19);
+            lblTitulo.Location = new Point(410, 32);
+            lblTitulo.Margin = new Padding(4, 0, 4, 0);
             lblTitulo.Name = "lblTitulo";
-            lblTitulo.Size = new Size(215, 25);
+            lblTitulo.Size = new Size(325, 40);
             lblTitulo.TabIndex = 5;
             lblTitulo.Text = "Mostrar ruta mas rápida";
             // 
@@ -100,47 +105,54 @@
             gbxRuta.Controls.Add(cbxOrigen);
             gbxRuta.Controls.Add(cbxDestino);
             gbxRuta.Controls.Add(lblOrigen);
-            gbxRuta.Location = new Point(64, 62);
+            gbxRuta.Location = new Point(91, 103);
+            gbxRuta.Margin = new Padding(4, 5, 4, 5);
             gbxRuta.Name = "gbxRuta";
-            gbxRuta.Size = new Size(681, 170);
+            gbxRuta.Padding = new Padding(4, 5, 4, 5);
+            gbxRuta.Size = new Size(973, 283);
             gbxRuta.TabIndex = 6;
             gbxRuta.TabStop = false;
             // 
             // btnConsultar
             // 
             btnConsultar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnConsultar.Location = new Point(282, 110);
+            btnConsultar.Location = new Point(403, 183);
+            btnConsultar.Margin = new Padding(4, 5, 4, 5);
             btnConsultar.Name = "btnConsultar";
-            btnConsultar.Size = new Size(100, 40);
+            btnConsultar.Size = new Size(143, 67);
             btnConsultar.TabIndex = 4;
             btnConsultar.Text = "Consultar";
             btnConsultar.UseVisualStyleBackColor = true;
+            btnConsultar.Click += btnConsultar_Click;
             // 
             // lblRecorridos
             // 
             lblRecorridos.AutoSize = true;
             lblRecorridos.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblRecorridos.Location = new Point(396, 19);
+            lblRecorridos.Location = new Point(566, 32);
+            lblRecorridos.Margin = new Padding(4, 0, 4, 0);
             lblRecorridos.Name = "lblRecorridos";
-            lblRecorridos.Size = new Size(178, 25);
+            lblRecorridos.Size = new Size(268, 40);
             lblRecorridos.TabIndex = 7;
             lblRecorridos.Text = "Recorrido de la ruta";
             // 
             // lbxRuta
             // 
             lbxRuta.FormattingEnabled = true;
-            lbxRuta.Location = new Point(310, 47);
+            lbxRuta.Location = new Point(443, 78);
+            lbxRuta.Margin = new Padding(4, 5, 4, 5);
             lbxRuta.Name = "lbxRuta";
-            lbxRuta.Size = new Size(351, 94);
+            lbxRuta.Size = new Size(500, 154);
             lbxRuta.TabIndex = 8;
             // 
             // lblTiempoEst
             // 
             lblTiempoEst.AutoSize = true;
             lblTiempoEst.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblTiempoEst.Location = new Point(9, 19);
+            lblTiempoEst.Location = new Point(13, 32);
+            lblTiempoEst.Margin = new Padding(4, 0, 4, 0);
             lblTiempoEst.Name = "lblTiempoEst";
-            lblTiempoEst.Size = new Size(166, 25);
+            lblTiempoEst.Size = new Size(253, 40);
             lblTiempoEst.TabIndex = 9;
             lblTiempoEst.Text = "Tiempo Estimado: ";
             // 
@@ -151,9 +163,11 @@
             gbxResultado.Controls.Add(lblTiempoEst);
             gbxResultado.Controls.Add(lbxRuta);
             gbxResultado.Controls.Add(lblRecorridos);
-            gbxResultado.Location = new Point(64, 250);
+            gbxResultado.Location = new Point(91, 417);
+            gbxResultado.Margin = new Padding(4, 5, 4, 5);
             gbxResultado.Name = "gbxResultado";
-            gbxResultado.Size = new Size(681, 158);
+            gbxResultado.Padding = new Padding(4, 5, 4, 5);
+            gbxResultado.Size = new Size(973, 263);
             gbxResultado.TabIndex = 10;
             gbxResultado.TabStop = false;
             // 
@@ -161,9 +175,10 @@
             // 
             lblComplMinutos.AutoSize = true;
             lblComplMinutos.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblComplMinutos.Location = new Point(117, 67);
+            lblComplMinutos.Location = new Point(167, 112);
+            lblComplMinutos.Margin = new Padding(4, 0, 4, 0);
             lblComplMinutos.Name = "lblComplMinutos";
-            lblComplMinutos.Size = new Size(91, 25);
+            lblComplMinutos.Size = new Size(139, 40);
             lblComplMinutos.TabIndex = 11;
             lblComplMinutos.Text = "Minutos ";
             // 
@@ -171,18 +186,20 @@
             // 
             lblMinutos.AutoSize = true;
             lblMinutos.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblMinutos.Location = new Point(36, 67);
+            lblMinutos.Location = new Point(51, 112);
+            lblMinutos.Margin = new Padding(4, 0, 4, 0);
             lblMinutos.Name = "lblMinutos";
-            lblMinutos.Size = new Size(39, 25);
+            lblMinutos.Size = new Size(56, 40);
             lblMinutos.TabIndex = 10;
             lblMinutos.Text = "***";
             // 
             // btnCerrar
             // 
             btnCerrar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnCerrar.Location = new Point(610, 421);
+            btnCerrar.Location = new Point(871, 702);
+            btnCerrar.Margin = new Padding(4, 5, 4, 5);
             btnCerrar.Name = "btnCerrar";
-            btnCerrar.Size = new Size(100, 40);
+            btnCerrar.Size = new Size(143, 67);
             btnCerrar.TabIndex = 5;
             btnCerrar.Text = "Cerrar";
             btnCerrar.UseVisualStyleBackColor = true;
@@ -191,9 +208,10 @@
             // btnLimpiar
             // 
             btnLimpiar.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnLimpiar.Location = new Point(161, 421);
+            btnLimpiar.Location = new Point(230, 702);
+            btnLimpiar.Margin = new Padding(4, 5, 4, 5);
             btnLimpiar.Name = "btnLimpiar";
-            btnLimpiar.Size = new Size(100, 40);
+            btnLimpiar.Size = new Size(143, 67);
             btnLimpiar.TabIndex = 11;
             btnLimpiar.Text = "Limpiar";
             btnLimpiar.UseVisualStyleBackColor = true;
@@ -201,14 +219,15 @@
             // 
             // frmRutaRapida
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 473);
+            ClientSize = new Size(1143, 788);
             Controls.Add(btnLimpiar);
             Controls.Add(btnCerrar);
             Controls.Add(gbxResultado);
             Controls.Add(gbxRuta);
             Controls.Add(lblTitulo);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "frmRutaRapida";
             Text = "Ruta mas Rápida";
             gbxRuta.ResumeLayout(false);
--- Proyecto_EDII_Avila_Cortes_Henriquez/frmRutaRapida.cs
+++ Proyecto_EDII_Avila_Cortes_Henriquez/frmRutaRapida.cs
@@ -13,6 +13,19 @@ namespace Proyecto_EDII_Avila_Cortes_Henriquez
         public frmRutaRapida()
         {
             InitializeComponent();
+            this.Load += frmRutaRapida_Load;
+            btnConsultar.Click += btnConsultar_Click;
+        }
+        private void frmRutaRapida_Load(object sender, EventArgs e)
+        {
+            cbxOrigen.Items.Clear();
+            cbxDestino.Items.Clear();
+
+            foreach (var nodo in Global.Ciudad.ObtenerGrafo().Keys)
+            {
+                cbxOrigen.Items.Add(nodo);
+                cbxDestino.Items.Add(nodo);
+            }
         }
 
         private void btnCerrar_Click(object sender, EventArgs e)
@@ -31,6 +44,27 @@ namespace Proyecto_EDII_Avila_Cortes_Henriquez
             cbxDestino.SelectedIndex = -1;
             lblMinutos.Text = "***";
             lbxRuta.Items.Clear();
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            if (cbxOrigen.SelectedItem == null ||
+       cbxDestino.SelectedItem == null)
+            {
+                MessageBox.Show("Seleccione origen y destino");
+                return;
+            }
+
+            string origen = cbxOrigen.SelectedItem.ToString();
+            string destino = cbxDestino.SelectedItem.ToString();
+
+            int minutos = Global.Ciudad.Dijkstra(origen, destino);
+
+            lblMinutos.Text = minutos.ToString();
+
+            lbxRuta.Items.Clear();
+            lbxRuta.Items.Add(origen);
+            lbxRuta.Items.Add(destino);
         }
     }
 }
